796bcbf0dcd84ce9670811981ffbd3f6d4a458d4
Khoa10802 <73599197+Khoa10802@users.noreply.github.com>
2023-04-12 15:29:59 +0700

    Add files via upload

---
+++ T4 - OOP/src/Inheritance/ElectronicProduct.java
@@ -0,0 +1,8 @@
+package Inheritance;
+
+public class ElectronicProduct extends Product {
+    @Override
+    public void productInfo() {
+        System.out.println("Electronic Quality");
+    }
+}
---
+++ T4 - OOP/src/Inheritance/FBProduct.java
@@ -0,0 +1,8 @@
+package Inheritance;
+
+public class FBProduct extends Product {
+    @Override
+    public void productInfo() {
+        System.out.println("FB Quality");
+    }
+}
